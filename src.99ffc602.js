2143bf689351c6aa0e97a5bf7eff08b963bf1fd4
rslnbyk <74673598+rslnbyk@users.noreply.github.com>
2022-05-12 18:49:15 +0000

    Deploying to gh-pages from @ rslnbyk/apello-parcel@97f38ee862fa790e05e2a11637698be26feb70e2 🚀

--- src.99ffc602.js
+++ src.99ffc602.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/apello-parcel/src.36e6fbd6.js.map
+//# sourceMappingURL=/apello-parcel/src.99ffc602.js.map
